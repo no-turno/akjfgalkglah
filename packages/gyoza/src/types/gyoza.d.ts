8b5aa546d57491387773c332da1bbdc95f3473f2
Davi <davispindola2828@gmail.com>
2024-05-25 04:20:07 +0000

    push

--- packages/gyoza/src/types/gyoza.d.ts
+++ packages/gyoza/src/types/gyoza.d.ts
@@ -1,1 +1,35 @@
-declare module "gyoza" {}
+export interface Server { }
+
+export interface Client { }
+
+export interface Route {
+	name: string;
+	handler<T extends (Client | Server)>(mod: T): {
+		module: T,
+		platform: Set<{
+			readonly type: 'member',
+			readonly isBoot: boolean
+		}>
+	}
+}
+
+export interface Gyoza<__client__ = Server, __server__ = Server> {
+	System: {
+		client: __client__,
+		server: __server__,
+		routers: Set<Route>
+	}
+}
+
+declare namespace Gyoza {
+	export {
+		Server,
+		Client,
+		Route,
+		Gyoza
+	}
+}
+
+declare module "gyoza" {
+	
+}
